9728c1dda61ee4f043f5122416e14292e58b5948
laumonmay <laumonmay@hotmail.com>
2023-12-21 21:29:18 +0100

    actualizacion

--- js/main.js
+++ js/main.js
@@ -16,19 +16,42 @@ const list = document.querySelector(".js_ul");
 
 // const  = document.querySelector('.js_btn');
 
-//   for( const tasksItem of tasks) {
-//     list.innerHTML += `<li class="li js_li"><input type="checkbox">${tasksItem.name}</li>`;
-// }
+/*
+for( const tasksItem of tasks) {
+  list.innerHTML += `<li class="li js_li"><input class="js_check" type="checkbox" value="">${tasksItem.name}</li>`;
+}
+*/
 
-list.innerHTML = `<li class="li js_li"><input class="js_check" type="checkbox"/>${tasks[1].name}</li>`;
-
-/// evento si hacemos click en checkbox la tarea es completada,(if) si es completada se añade la clase tachado (classlist.add)
-
-const checkBox = document.querySelector(".js_check");
-const eachItem = document.querySelector(".js_li");
-function handleChecK(ev) {
-  console.log("hola");
-  if (tasks[1].completed) eachItem.classList.add("tachado");
+for (let i = 0; i < tasks.length; i++) {
+  list.innerHTML += `<li class="li js_li"><input class="js_check" type="checkbox" value="${i}">${tasks[i].name}</li>`;
 }
 
-checkBox.addEventListener("click", handleChecK);
+// list.innerHTML = `<li class="li js_li"><input class="js_check" type="checkbox"/>${tasks[1].name}</li>`;
+
+/// evento si hacemos click en checkbox la tarea es completada,(if) si es completada se añade la clase tachado (classlist.add)
+// hacemos una variable que englobe a todos los checkboxs
+const checkBoxs = document.querySelectorAll(".js_check");
+const eachItem = document.querySelector(".js_li");
+
+function handleChecK(event) {
+  console.log("hola");
+  console.log(event.target.value);
+  if (tasks[event.target.value].completed) {
+    //eachItem.classList.toggle("tachado");
+    tasks[event.target.value].completed = false;
+  } else {
+    tasks[event.target.value].completed = true;
+  }
+  for (let i = 0; i < tasks.length; i++) {
+    list.innerHTML += `<li class="li js_li"><input class="js_check" type="checkbox" value="${i}">${tasks[i].name}</li>`;
+  }
+  for (const checkBox of checkBoxs) {
+    checkBox.addEventListener("click", handleChecK);
+  }
+}
+
+for (const checkBox of checkBoxs) {
+  checkBox.addEventListener("click", handleChecK);
+}
+
+/// cuando esta completed
